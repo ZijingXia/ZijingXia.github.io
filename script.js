0a8811a0020b9d4336aa5f58cc2e75bcb62e9fad
ZijingXia <xiazijing714@gmail.com>
2026-09-03 21:02:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function slide(sliderId, direction) {
 
+    const slider = document.getElementById(sliderId);
+
+    if (!slider) {
+        return;
+    }
+
+    const amount = 390;
+
+    slider.scrollBy({
+        left: amount * direction,
+        behavior: "smooth"
+    });
+}
